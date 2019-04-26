a9d06159e4c5df5924be4760a513c4a791102235
vjose <joseluisv1201@gmail.com>
2019-04-26 20:05:23 +0200

    System Discretized

--- project/compute_controller_base_parameters.m
+++ project/compute_controller_base_parameters.m
@@ -4,8 +4,25 @@ function param = compute_controller_base_parameters
     
     % (2) discretization
     Ts = 60;
-    A = ...
-    B = ...
+
+    % Parameter vector p
+    p = truck;
+
+    A_c(1,1) = (-p.a12 - p.a1o)/p.m1;
+    A_c(1,2) = p.a12/p.m1;
+    A_c(1,3) = 0;
+    A_c(2,1) = p.a12/p.m2;
+    A_c(2,2) = (-p.a12-p.a23-p.a2o)/p.m2;
+    A_c(2,3) = p.a23/p.m2;
+    A_c(3,1) = 0;
+    A_c(3,2) = p.a23/p.m3;
+    A_c(3,3) = (-p.a23-p.a3o)/p.m3;
+
+    B_c = [1/p.m1 0; 0 1/p.m2; 0 0];
+    sys_continuous = ss(A_c, B_c, [1, 1, 1], 0);
+    sys_discrete = c2d(sys_continuous, Ts);
+    A = sys_discrete.A;
+    B = sys_discrete.B;
     
     % (3) set point computation
     T_sp = ...
--- project/run_simulations.m
+++ project/run_simulations.m
@@ -2,10 +2,13 @@
 clear all
 close all
 addpath(genpath(cd));
-load('system/parameters_scenarios.mat');
+load('project/system/parameters_scenarios.mat');
+load('project/system/parameters_truck.mat');
 
 %% E.g. execute simulation with LQR
 % clear persisten variables of function controller_lqr
 clear controller_lqr; 
+
 % execute simulation starting from T0_1 using lqr controller with scenario 1
+T0_1 = [1, 1, 1];
 [T, p] = simulate_truck(T0_1, @controller_lqr,scen1);
